cdccb7ad6a27ed2202dfe2624dfc2224452f215e
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-04-02 14:41:53 +0800

    ✨ Add Basic layout and function

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,188 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Dropzone, DropzoneStatus } from "@mantine/dropzone";
+import {
+  Button,
+  Container,
+  Grid,
+  Group,
+  Image,
+  MantineTheme,
+  NumberInput,
+  Text,
+  useMantineTheme
+} from '@mantine/core';
+import {
+  Icon as TablerIcon,
+  Loader,
+  Photo,
+  Upload,
+  X
+} from 'tabler-icons-react';
+import { ComponentProps, useEffect, useState } from "react";
+import { showNotification } from "@mantine/notifications";
+
+function getIconColor(status: DropzoneStatus, theme: MantineTheme) {
+  return status.accepted
+    ? theme.colors[theme.primaryColor][4]
+    : status.rejected
+      ? theme.colors.red[4]
+      : theme.colors.dark[0];
+}
+
+function ImageUploadIcon({ status, ...props }: ComponentProps<TablerIcon>
+  & { status: DropzoneStatus }) {
+  if (status.accepted) return <Upload {...props} />;
+  if (status.rejected) return <X {...props} />;
+  return <Photo {...props} />;
+}
+
+const dropzoneChildren = (status: DropzoneStatus, theme: MantineTheme) => (
+  <Group
+    position="center"
+    spacing="xl"
+    style={{ minHeight: 220, pointerEvents: 'none' }}
+  >
+    <ImageUploadIcon
+      status={status}
+      style={{ color: getIconColor(status, theme) }}
+      size={80}
+    />
+    <div>
+      <Text size="xl" color="white">
+        Drag your origin image here
+      </Text>
+      <Text size="sm" color="dimmed" inline mt={7}>
+        File must be .bmp format and size less than 5 MB.
+      </Text>
+    </div>
+  </Group>
+);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const theme = useMantineTheme();
+  const [id, setId] = useState<string>();
+  const [step, setStep] = useState(0);
+  const [level, setLevel] = useState(2);
+  const [ratio, setRatio] = useState(10);
+  const [isLoadingImage, setLoadingImage] = useState(false);
+
+  async function upload(files: File[]) {
+    try {
+      const body = new FormData();
+      body.append('image', files[0]);
+      const res = await fetch('https://api.haar.linyuanlin.com/upload',
+        { method: 'POST', body });
+      const parsed = await res.json();
+      showNotification({
+        title: '上传成功',
+        message: 'uid 为 ' + parsed.id,
+      })
+      setId(parsed.id);
+    } catch (err) {
+      showNotification({
+        color: 'red',
+        title: 'Default notification',
+        message: 'Hey there, your code is awesome! 🤥',
+      })
+    }
+  }
+
+  useEffect(() => {
+    setLoadingImage(true);
+  }, [level, step, ratio, id]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div style={{ width: "100%", height: '100vh', position: 'relative' }}>
+      <Container size="sm" px="sm" mt={64}>
+        <Text color="white" weight="bolder" size="xl" align="center" mb={32}>
+          Haar Wavelet Image Compression Visualization
+        </Text>
+        <Grid mb={24}>
+          <Grid.Col span={6}>
+            <NumberInput
+              value={level}
+              onChange={v => {
+                setLevel(v || 0);
+                setStep(0);
+              }}
+              max={5}
+              min={1}
+              defaultValue={2}
+              label="level"
+            />
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <NumberInput
+              value={ratio}
+              onChange={v => setRatio(v || 0)}
+              min={0}
+              max={100}
+              parser={(value) => value?.replace(/\$\s?|(%*)/g, '')}
+              defaultValue={10}
+              formatter={(value) => `${value}%`}
+              label="Compression ratio"
+            />
+          </Grid.Col>
+        </Grid>
+        {!id && <div>
+            <Dropzone
+                onDrop={upload}
+                onReject={(files) => console.log('rejected files', files)}
+                maxSize={3 * 1024 ** 2}
+                mb={24}
+                accept={['image/bmp']}
+            >
+              {(status) => dropzoneChildren(status, theme)}
+            </Dropzone>
+            <a href={"/example.bmp"} download
+               style={{ color: 'white', textAlign: 'center' }}>
+                Don't have .bmp file? Download example from here !
+            </a>
+        </div>}
+        {id && <div>
+            <Image
+                onError={() => showNotification({
+                  color: 'red',
+                  title: 'Error occurred',
+                  message: 'There is an issue during processing or loading your image, please open an issue on GitHub.',
+                })}
+                onLoad={() => setLoadingImage(false)}
+                placeholder={<Loader />}
+                height="50vh"
+                fit="contain"
+                src={`https://api.haar.linyuanlin.com/download?uid=${id}&step=${step}&level=${level}&ratio=${ratio}`}
+                alt="" />
+            <Grid justify="space-between" mt={16}>
+                <Grid.Col span={4}>
+                    <Button
+                        fullWidth
+                        disabled={step === 0 || isLoadingImage}
+                        onClick={() => setStep(step - 1)}
+                    >
+                        Previous Step
+                    </Button>
+                </Grid.Col>
+                <Grid.Col span={4}>
+                    <Button
+                        fullWidth
+                        disabled={step >= level * 4 + 1 || isLoadingImage}
+                        onClick={() => setStep(step + 1)}
+                    >
+                        Next Step
+                    </Button>
+                </Grid.Col>
+            </Grid>
+        </div>}
+      </Container>
+      <div style={{ position: 'absolute', bottom: 96, width: '100%' }}>
+        <Text
+          color="dimmed" weight="bolder" align="center"
+          style={{ cursor: 'pointer' }}
+          mt={32} underline onClick={() => {
+          window.open('https://github.com/ken20001207/haar-wavelet-image-compression-visualization')
+        }}>
+          Made with ❤️ by Yuanlin Lin
+        </Text>
+      </div>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
+import { MantineProvider } from '@mantine/core'
+import { NotificationsProvider } from '@mantine/notifications'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={{ colorScheme: 'dark' }}>
+      <NotificationsProvider>
+        <App />
+      </NotificationsProvider>
+    </MantineProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
